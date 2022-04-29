03686c80ff99c64f857c30cc3711316382396096
sunha <sunha0928@naver.com>
2022-04-30 01:34:26 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://ahnus:qhfltkfkd1@boilerplate.fwc0n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
@@ -9,7 +14,21 @@ mongoose.connect('mongodb+srv://ahnus:qhfltkfkd1@boilerplate.fwc0n.mongodb.net/m
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('방가방가 하이루')
+})
+
+app.post('/register', (req, res) => {
+  // 회원가입 시 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 삽입
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false. err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
